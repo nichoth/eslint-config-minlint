5bd477eb185d4c4e6aecd24cbd18a4d210578445
Nick Thomas <nichoth@gmail.com>
2016-12-23 11:29:38 -0800

    ignore max len for strings

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ module.exports = {
         "no-unused-vars": [2, {"args": "none"}],
         "no-unreachable": [2],
         "arrow-parens": [2, "as-needed"],
-        "max-len": [2, 80],
+        "max-len": [2, {
+            "code": 80,
+            "ignoreStrings": true
+        }],
         "no-undef": [2]
     }
 }
